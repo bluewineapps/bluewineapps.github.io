58b73404b9c979337f3424da02baf022157788a7
Midhun <midhun@saal.ai>
2020-03-27 18:04:52 +0400

    added ti-tag game

---
+++ tic-tac/precache-manifest.0408c7a26b2f66a669cc3a4ebd4d0cab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bbf7251d528bf5f0c1044b93b0f6dfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caac89c7f2f9957c672b",
+    "url": "/static/css/main.ed8b2b58.chunk.css"
+  },
+  {
+    "revision": "f4e8c56b9a0615e88f2b",
+    "url": "/static/js/2.9f1d3c16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f1d3c16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caac89c7f2f9957c672b",
+    "url": "/static/js/main.fbc5c977.chunk.js"
+  },
+  {
+    "revision": "72d21ae4b90647bd685d",
+    "url": "/static/js/runtime-main.bcfe671f.js"
+  }
+]);
